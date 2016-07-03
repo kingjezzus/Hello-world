a1c1f0c1e6f4cf9b1c0116ddb9be3ccc56ed795c
kingjezzus <jesusrizo548@gmail.com>
2016-07-02 22:18:09 -0700

    Update simple.c

--- simple.c
+++ simple.c
@@ -4,3 +4,34 @@ int main(int argc, char *argv[]){
 print("Hello World\n")
 main 0; 
 }
+<!DOCTYPE html>
+<html>
+<body>
+<div class="dropdown"> 
+ 
+ <div id="logo" class="sixteen columns">
+ <h3>My Dreams</h3>
+ </div>
+ 
+ <div id="nav" class="sixteen columns">
+ <ul>
+ <li><a href="#">Home</a>This may look impossible but people have dreams</li>
+ <li><a href="#">Dream one</a></li>
+ <li><a href="#">Dream two</a></li>
+ <li><a href="#">Dream three</a></li>
+ </ul>
+ </div>
+ 
+ <div id="theText" class="eight columns">
+ <h1>No One is Going To Stop Me</h1>
+ <p>My Name is...</p>
+ 
+ <p>Jesus Herrera</p>Well, I may not be the best person out there in the world but I could say that since I was small I DREAMED BIG and have always and will always. First, A dream that many people tell me that i will never accomplish is becoming one of the biggest idols out there and having othres look up to me for the things I will do for this world to become better. MY second Dream may not be a dream to many people have but, is to build a school...I know it may sound impossible but, I always dreamed of buildimg an amazing school that will become one of the will know school in the world. My third dream... Will it may sound impossible but, is to build places were homless ,immigrant families and poor families to go to were they can recieve help and volunteers can help teach them amd help them get back on thier feet until they are ready to go back to the real world .
+ </div>
+ 
+ <div id="bigphoto" class="seven columns add-bottom offset-by-one">
+ <img src="https://lh3.googleusercontent.com/-L3owaJFKVRU/VxmsCsr0zYI/AAAAAAAAAz4/2JlIKUn4BTw3omeefvTj1MJNV5hlD97sgCKgB/h120/IMG_0329.JPG"/>
+ </div>
+ 
+ 
+</div><!-- container -->
